7836a2f247d3fe276f56f06013eb3bc1e97b9cbe
Renan Damascena Pereira <renandamascena@gmail.com>
2022-12-16 21:05:34 -0300

    acesso ao app

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,47 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 export default function Login(){
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+    const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login"
+    const navigate = useNavigate()
+
+
+    function makeLogin(event){
+        event.preventDefault()
+        setLoading(true)
+
+        const request = axios.post(url,{
+            email:email,
+            password:password
+        })
+        request.then(()=>navigate("/hoje"))
+    }
+
+
     return(
         <Screen>
         
         <img src="assets/TrackIt.png" alt=""/>
-        <input type="text" placeholder="email"/>
-        <input type="text" placeholder="senha"/>
-        <Link to="/habitos"><button>Entrar</button></Link>
+        <form onSubmit={makeLogin}>
+            <input type="text"required
+            value={email} 
+            onChange={e=> setEmail(e.target.value)} 
+            placeholder="email"/>
+
+            <input type="password" required 
+            value={password} 
+            onChange={e=>setPassword(e.target.value)}
+            placeholder="senha"/>
+
+            <button type="submit" disabled={loading}>{loading ? <img src="loading1.svg" alt="loading"/> : "Entrar" }</button>
+        </form>
         <Link to="/cadastro"><div>Não tem uma conta? Cadastre-se!</div></Link>
         </Screen>
     )
@@ -30,8 +63,10 @@ const Screen = styled.div`
         border-radius: 5px;
         margin: 3px;
         color: #666666;
+        background-color: ${props=> props.loading ? "#F2F2F2" : "white"};
         font-size: 20px;
         padding: 0px 11px;
+        box-sizing: border-box;
     }
     input::placeholder{
         margin: 0px 11px;
@@ -43,6 +78,7 @@ const Screen = styled.div`
         height: 45px;
         color: white;
         background-color: #52B6FF;
+        opacity: ${props=> props.loading ? "0.7" : "1"};
         border: none;
         border-radius: 5px;
         margin: 3px;
@@ -53,5 +89,10 @@ const Screen = styled.div`
         text-decoration-line: none;
         margin-top: 22px;
         
+    }
+    form{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 `
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,16 +1,42 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Registration(){
+
+    const [email, setEmail] = useState("")
+    const [ password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [image, setImage] = useState("")
+    const navigate = useNavigate()
+    const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up"
+
+    function createAnAccount(event){
+        event.preventDefault()
+
+        const request = axios.post(url,{
+            email:email,
+            name:name,
+            image:image,
+            password:password
+        })
+        
+        request.then(()=> navigate("/"))
+    }
+
+
     return(
         <Screen>
         
         <img src="assets/TrackIt.png" alt=""/>
-        <input type="text" placeholder="email"/>
-        <input type="password" placeholder="senha"/>
-        <input type="text" placeholder="nome"/>
-        <input type="text" placeholder="foto"/>
-        <button>Entrar</button>
+        <Form onSubmit={createAnAccount}>
+        <input type="text" required value={email} onChange={e=> setEmail(e.target.value)}  placeholder="email"/>
+        <input type="password" required value={password} onChange={e=> setPassword(e.target.value)} placeholder="senha"/>
+        <input type="text" value={name} onChange={e=> setName(e.target.value)} placeholder="nome"/>
+        <input type="text" value={image} onChange={e=> setImage(e.target.value)}  placeholder="foto"/>
+        <button type="submit">Entrar</button>
+        </Form>
         <Link to="/"><div>Já tem uma conta? Faça login!</div></Link>
         </Screen>
     )
@@ -21,6 +47,7 @@ const Screen = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-top: -70px;
     img{
         width: 180px;
         margin: 68px 0px 34px 0px;
@@ -56,4 +83,10 @@ const Screen = styled.div`
         margin-top: 22px;
         
     }
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
